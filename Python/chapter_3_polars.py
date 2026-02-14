fea21f26393aa295a30eb87e90cfb7f6244f67c0
Sungwook Choi <swchoi75@gmail.com>
2026-02-14 21:23:11 +0900

    update chapter_3.py files

--- Python/chapter_3_polars.py
+++ Python/chapter_3_polars.py
@@ -1,3 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
 # %%
 import polars as pl
 from lets_plot import *
